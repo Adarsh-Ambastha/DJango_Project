7c44b6484978130fd8480ee1224ab420dba1885c
Adarsh <adarshambastha18@gmail.com>
2021-02-25 20:18:40 +0530

    Created Cars app and fetch the data from database

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from .models import Team
+from cars.models import Car
 # Create your views here.
 
 def home(request):#whenver we are clicking on a url we are making a
-    return render(request, 'pages/home.html')#render means throwing
+    teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('created_date').filter(is_featured = True)
+    all_cars = Car.objects.order_by('created_date')
+    data ={
+        'teams' : teams,
+        'featured_cars':featured_cars,
+        'all_cars' : all_cars,
+        }
+    return render(request, 'pages/home.html',data)#render means throwing
 def about(request):
     return render(request, 'pages/about.html')
 def services(request):
